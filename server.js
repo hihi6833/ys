e4c9afd2e6f632ae90cf25d71aa2d927adee3dc8
hihi6833 <hihi6833@naver.com>
2024-08-24 00:03:20 +0900

    Add files via upload

---
+++ server.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// 'public' 폴더를 정적 파일 제공을 위해 사용
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'main.html'));
+});
+
+app.get('/breakfast', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'breakfast.html'));
+});
+
+app.get('/snack', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'snack.html'));
+});
+
+app.get('/convenience', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'convenience.html'));
+});
+
+app.get('/healthy', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'healthy.html'));
+});
+
+app.get('/midnight', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'midnight.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
